7066c5d5757b22f5055051d09e0101df942d1742
Genuel Mark <gmzapanta98@gmail.com>
2023-05-05 19:01:43 +1000

    added classes that return pass

---
+++ src/rooms.py
@@ -0,0 +1,23 @@
+class entrance():
+    # Starting room 
+    pass
+
+class exitRoom():
+    # Basic room to exit from the dungeon probably boolean return for quit
+    pass
+
+class skeletalRoom():
+
+    pass
+
+class ghostRoom():
+    #n Next to a death room, scary text and exit opposite
+    pass
+
+class scaryRoom():
+    pass
+
+class Fight():
+    pass
+
+# Extras
