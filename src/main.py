2841d1041bab8841b41e5b42278bf3d4a43f7d07
Genuel Mark <gmzapanta98@gmail.com>
2023-05-08 17:22:38 +1000

    connected main to rooms python classes file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-import rooms
+from rooms import rooms
+
+# Create instance of it
+rooms = rooms()
 
 # Main
 
@@ -6,14 +9,24 @@ import rooms
 # 2. Ask player for their name
 PLAYER_NAME = input("Welcome to the game! Please enter your name: ")
 # 3. Ask player using name if they want to play
-game_state = (f'{PLAYER_NAME} would you like to play? (y/n)')
+game_state = input(f'{PLAYER_NAME} would you like to play? (y/n)\n')
 # 4. If yes change game to True 
-while game_state == 'True':
-    print ('You are playing')
-    break
+if game_state == 'y':
+    while True:
+        rooms.createRoom()
+
+ 
+
+
+
+
+
+
+
+
     # 4.1. Start loop
         # 4.1.a.If player dies ask to play again
         # 4.1.b If player escapes ask to play again
             # if player says no then quit game
 # . If not set game to False and quit the application
-print (f'{PLAYER_NAME}, thank you for playing!')
+# print (f'{PLAYER_NAME}, thank you for playing!')
