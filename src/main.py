04ee77845d8e711c0ac1eacaa88f59d5423ac3a3
Genuel Mark <gmzapanta98@gmail.com>
2023-05-08 11:52:17 +1000

    added basic pseudocode and functionality to main for testing

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
 import rooms
+
+# Main
+
+# 1. When player opens game welcome to the game
+# 2. Ask player for their name
+PLAYER_NAME = input("Welcome to the game! Please enter your name: ")
+# 3. Ask player using name if they want to play
+game_state = (f'{PLAYER_NAME} would you like to play? (y/n)')
+# 4. If yes change game to True 
+while game_state == 'True':
+    print ('You are playing')
+    break
+    # 4.1. Start loop
+        # 4.1.a.If player dies ask to play again
+        # 4.1.b If player escapes ask to play again
+            # if player says no then quit game
+# . If not set game to False and quit the application
+print (f'{PLAYER_NAME}, thank you for playing!')
