e1bc0d5054a4e6b4e00123215cb7262928a7d2ad
Genuel Mark <gmzapanta98@gmail.com>
2023-05-10 13:42:14 +1000

    overhauled code to allow weapon to be accessible

--- src/rooms.py
+++ src/rooms.py
@@ -1,88 +1,116 @@
+import csv
+import random
+
+# Global available variable since its just for reading through csv files
+random_number = str(random.randint(10000000, 99999999))
+# write to file 
+with open('random_number.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow([random_number])
+
 class player():
     def __init__(self, weapon=False):
         # initiates weapon and sets to False
-        self.weapon = weapon if weapon is not None else False 
-        # Sets weapon to false if weapon is called and no argument is provided 
+        self.weapon = weapon
 
     def get_weapon(self):
         self.weapon = True # sets weapon to True when called
 
-class rooms(): 
 
-    def weaponRoom():
-        # makes a player instance without a weapon
-        weapon = player()
+class rooms:
+    def __init__(self):
+        # creates usable instance of player for weapon
+        self.player = player()
 
-        if not weapon.weapon:
-            print ("You see the hilt of a blade lodged into a wall")
-
+    # Right side of the main room
+    def weaponRoom(self):
+        # check if player weapon = True
+        if not self.player.weapon:
+            print("You see the hilt of a blade lodged into a wall")
             pick_up = input("Do you pick up the weapon? (y/n): ")
             if pick_up == 'y':
-                weapon.get_weapon()
+                self.player.get_weapon()
                 print("You pick up the weapon and now have a weapon!")
-            rooms.skeletalRoom()
+                self.skeletalRoom()
+            else:
+                print("No")
         else:
-            print ("You see the hole where the blade used to be. Other that that, it's just a dead end wall")
-            rooms.skeletalRoom()
+            print("You see the hole where the blade used to be. Other than that, it's just a dead end wall")
+            self.skeletalRoom()
     
-    def skeletalRoom(): # Skeletal room
-        choice = input ("Skeletal Room can choose 'forward' or 'left'")
+    def skeletalRoom(self):
+        choice = input ("Skeletal Room can choose 'forward' or 'left' or 'back': ")
 
         if choice == 'forward':
             print ("The door ahead opens into a dead end wall and go back to the previous room")
-            rooms.skeletalRoom()
-        if choice == 'left':
-            rooms.weaponRoom()
+            self.skeletalRoom()
+        elif choice == 'left':
+            self.weaponRoom()
+        elif choice == 'back':
+            self.createRoom()
 
-
-    def monsterRoom():
-        weapon = player(weapon)
-        print ("As you enter the room you hear a grunt and eyes staring into your soul.\n ITS A MONSTER!" )
-        weapon 
-        fight_flee = input("Do you 'fight' or 'flee'?\n")
-        if (fight_flee == 'fight' and not weapon.weapon):
-            print ("YOU HAVE NO WEAPON AND DIED")
+    def monsterRoom(self):
+        print("As you enter the room you hear a grunt and eyes staring into your soul. ITS A MONSTER!")
+        fight_flee = input("Do you 'fight' or 'flee'?: ")
+        if fight_flee == 'fight' and not self.player.weapon:
+            print("YOU HAVE NO WEAPON AND DIED")
             quit()
-        elif (fight_flee == 'fight' and  weapon.weapon):
-            print ("YOU HAVE ESCAPED")
+        elif fight_flee == 'fight' and self.player.weapon:
+            print("YOU HAVE ESCAPED")
             quit()
 
-
-    def scaryRoom():
-        print("You are in scary room go 'right' or 'left'\n")
+    # Forward from Main Room
+    def scaryRoom(self):
+        print("You are in scary room go 'right' or 'left': ")
         choice = input ("")
         if choice == 'right':
-            rooms.monsterRoom()
+            self.monsterRoom()
         if choice == 'left': # Instant Death
-            print ("As you walk into the room you hear crumbling and all of a sudden the floor collapses.\n You have died")
+            print("As you walk into the room you hear crumbling and all of a sudden the floor collapses. You have died")
             quit() # Needs to go back to loop to the welcome perhaps a boolean return?
 
+    # Puzzle Room
 
-    def createRoom(self):
-        choice = input ("You are in a room filled with torches with a few choices of direction.\n Do you go 'forward' 'back' 'left' or 'right'?\n")
+    def puzzleRoom(self):
+        choice = input("You are in a room filled with what seems to be images and some doors. Do you go 'forward' 'back' or 'right'?: ")
         if choice == "forward":
-            rooms.scaryRoom()
-        elif choice == "back":
-            print ("You attempt to use the back door of the room but it opens into a dead end")
-        elif choice == "right":
-            rooms.skeletalRoom()
-
-    # 2. Ask player to decide which direction to go
-    # 3. Depending on choice move to the room (right or forward)
-
-class exitRoom():
-    # Basic room to exit from the dungeon probably boolean return for quit
-
-    pass
+            self.puzzleClue()
+        elif choice == 'back':
+            self.createRoom()
+        elif choice == 'right':
+            self.puzzleEscape()
 
 
-class ghostRoom():
-    #n Next to a death room, scary text and exit opposite
-    pass
+    def puzzleEscape():
+        user_input = input ("As you look closer at the door there is a puzzle requiring a series of numbers to be entered. ")
+        # check if input is a 8 digit number
+        if len(user_input) == 8 and user_input.isdigit():
+            # Check if player is correct
+            if user_input == random_number:
+                print('Congratulations, your guess is correct!')
 
+            else:
+                print('Sorry, your guess is incorrect. Try again.\n')
+        else:
+            print('Invalid input. Please enter an 8-digit number or an interactable command.\n')
 
+    def puzzleClue():
+        choice = input('You see a book that looks like it relates to the door on the right. Read it? (y/n): ')
+        if choice == 'y':
+        # Read the random number from the CSV file and provide the first digit as a clue
+            with open('random_number.csv', mode='r') as file:
+                reader = csv.reader(file)
+                random_number = next(reader)[0]
+            print('A series of numbers is written over and over again, the number is: ', random_number)
 
-class Fight():
-    pass
-
-# Extras
+    # MAIN ROOM 
+    def createRoom(self):
+        choice = input("You are in a room filled with torches with a few choices of direction. Do you go 'forward' 'back' 'left' or 'right'?: ")
+        if choice == "forward":
+            self.scaryRoom()
+        elif choice == 'back':
+            print("You attempt to use the back door of the room but it opens into a dead end")
+        elif choice == 'right':
+            self.skeletalRoom()
+        elif choice == 'left':
+            self.puzzleRoom()
