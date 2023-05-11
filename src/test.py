3e163108992fd5b2782c0222543c225de0f918a9
Genuel Mark <gmzapanta98@gmail.com>
2023-05-11 18:02:30 +1000

    tests passed however nothing returned for monsterRoom()

--- src/test.py
+++ src/test.py
@@ -1,23 +1,18 @@
-from rooms import player, rooms
+from rooms import rooms
 import pytest
 
 def test_monster_room(monkeypatch):
     # Test that the monsterRoom function works as expected
-
     r = rooms()
-
+    monkeypatch.setattr('builtins.input', lambda _: "fight")  # Simulate player input
+    r.monsterRoom()
+    assert r.player.weapon == False
+    monkeypatch.setattr('builtins.input', lambda _: "flee")  # Simulate player input
+    r.monsterRoom()
+    assert r.player.weapon == False
+    monkeypatch.setattr('builtins.input', lambda _: "fight")  # Simulate player input
+    r.player.get_weapon()
+    assert r.player.weapon == True
+    monkeypatch.setattr('builtins.input', lambda _: "flee")  # Simulate player input
     r.monsterRoom()
     assert r.player.weapon == True
-
-    monkeypatch.setattr('builtins.input', lambda _: "fight")
-    i = input("What is your name?")
-    assert i == "fight"
-
-    r.monsterRoom()
-    assert r.player.weapon == True
-
-    r.monsterRoom("flee")
-    assert r.player.weapon == False
-
-    r.monsterRoom("fight")
-    assert r.player.weapon == False
