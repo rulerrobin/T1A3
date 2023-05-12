fd323d5b7d002f34bdb89eed603ca3fd77c9fcd1
Genuel Mark <gmzapanta98@gmail.com>
2023-05-12 14:21:57 +1000

    added validation code to all selections

--- src/rooms.py
+++ src/rooms.py
@@ -27,27 +27,32 @@ class rooms:
         # check if player weapon = True
         if not self.player.weapon:
             print("You see the hilt of a blade lodged into a wall")
-            pick_up = input("Do you pick up the weapon? (y/n): ")
-            if pick_up.lower() == 'y':
-                self.player.get_weapon()
-                print("You pick up the weapon and now have a weapon!")
-                self.skeletalRoom()
-            elif pick_up.lower() == 'n':
-                print("No")
-            else:
-                print ("Invalid input please use 'y' or 'n'")    
-        else:
+
+            while True: # loop until a valid choice is made
+                pick_up = input("Do you pick up the weapon? (y/n)\n")
+                if pick_up.lower() == 'y':
+                    self.player.get_weapon()
+                    print("You pick up the weapon and now have a weapon and return to the previous location")
+                    self.skeletalRoom()
+                elif pick_up.lower() == 'n':
+                    print("You decide to leave the hilt and go back to the previous location")
+                    self.skeletalRoom()
+                    break
+                else:
+                    print ("Invalid input please use 'y' or 'n'")    
+        else: # if player weapon is True
             print("You see the hole where the blade used to be. Other than that, it's just a dead end wall. You return to the previous room.")
             self.skeletalRoom()
     
     def skeletalRoom(self):
         allowed_choices = ['forward', 'left', 'back'] #list of possible choices
         while True:
-            choice = input("You are in a room filled with skeletons and are able to go 'forward' or 'left' or 'back': ")
+            choice = input("You are in a room filled with skeletons do you go 'forward' or 'left' or 'back'? \n")
             if choice.lower() in allowed_choices:
                 break
-            print("Invalid input, please choose one of the following: ", allowed_choices)
+            print("Invalid input, please use a valid option")
 
+        # once a valid choice is made
         if choice.lower() == 'forward':
             print("The door ahead opens into a dead end wall and go back to the previous room")
             self.skeletalRoom()
@@ -58,41 +63,51 @@ class rooms:
 
     def monsterRoom(self):
         print("As you enter the room you hear a grunt and eyes staring into your soul. ITS A MONSTER!")
-        fight_flee = input("Do you 'fight' or 'flee'?: ")
-        if fight_flee == 'fight' and not self.player.weapon:
-            print("YOU HAVE NO WEAPON AND DIED")
-            # quit()
-        elif fight_flee == 'flee' and not self.player.weapon:
-            print ("You escape the room and return to the previous location")
-            self.scaryRoom()
-        elif fight_flee == 'fight' and self.player.weapon:
-            print("YOU HAVE ESCAPED")
-            # quit()
-        elif fight_flee == 'flee' and self.player.weapon:
-            print ("You escape the room and return to the previous location")
-            self.scaryRoom()
+        while True:
+            fight_flee = input("Do you 'fight' or 'flee'?\n")
+            if fight_flee == 'fight' and not self.player.weapon:
+                print("YOU HAVE NO WEAPON AND DIED")
+                quit() # on death quit
+            elif fight_flee == 'flee' and not self.player.weapon:
+                print ("You escape the room and return to the previous location")
+                self.scaryRoom()
+            elif fight_flee == 'fight' and self.player.weapon:
+                print("YOU HAVE ESCAPED")
+                return
+            elif fight_flee == 'flee' and self.player.weapon:
+                print ("You escape the room and return to the previous location")
+                self.scaryRoom()
+            else:
+                print ("Please enter a valid input")
 
 
     # Forward from Main Room
     def scaryRoom(self):
-        print("You are in scary room go 'right' or 'left': ")
-        choice = input ("")
-        if choice == 'right':
-            self.monsterRoom()
-        if choice == 'left': # Instant Death
-            print("As you walk into the room you hear crumbling and all of a sudden the floor collapses. You have died")
-            quit() # Needs to go back to loop to the welcome perhaps a boolean return?
+        while True:
+            choice = input ("You are in scary room go 'right' 'left' or 'back'?\n ")
+            if choice == 'right':
+                self.monsterRoom()
+            elif choice == 'left': # Instant Death
+                print("As you walk into the room you hear crumbling and all of a sudden the floor collapses. You have died")
+                return
+            elif choice == 'back':
+                print ("You return to the starting room")
+            else:
+                print ("Please enter a valid input")
 
     # Puzzle Room
 
     def puzzleRoom(self):
-        choice = input("You are in a room filled with what seems to be images and some doors. Do you go 'forward' 'back' or 'right'?: ")
-        if choice == "forward":
-            self.puzzleClue()
-        elif choice == 'back':
-            self.createRoom()
-        elif choice == 'right':
-            self.puzzleEscape()
+        while True:
+            choice = input("You are in a room filled with what seems to be images and some doors. Do you go 'forward' 'back' or 'right'?\n")
+            if choice == "forward":
+                self.puzzleClue()
+            elif choice == 'back':
+                self.createRoom()
+            elif choice == 'right':
+                self.puzzleEscape()
+            else: 
+                print ("Please enter a valid input")
 
 
     def puzzleEscape(self):
@@ -101,18 +116,15 @@ class rooms:
 
         while guessing:
 
-            user_input = input ("As you look closer at the door there is a puzzle requiring a series of numbers to be entered. ")
-
-            # check if input is a 8 digit number
+            print ("As you look closer at the door and you try to fill in a 8 digit code)")
+            user_input = input ("Enter an 8 digit number \n")
             if len(user_input) == 8 and user_input.isdigit():
                 # Check if player is correct
                 if user_input == random_number:
                     print('As soon as you enter the numbers doors begin to move and you escape the dungeon!')
-                    print ('End') # debug
-                    return
 
                 else:
-                    again = input('Sorry, your guess is incorrect. Try again? (y/n): ')
+                    again = input('Sorry, your guess is incorrect. Try again? (y/n)\n')
                     if again.lower() == "n":
                         print ('You hear something above you turning as you return to the center of the room')
                         guessing = False
@@ -121,22 +133,25 @@ class rooms:
                         attempts += 1
                         print(f"You have {3-attempts} attempts left.")
 
-                        if attempts == 2:
+                        if attempts == 3:
                             break
                         else:
                             # reset guessing to True and prompt for input again
                             guessing = True
                     else:
-                        print("Invalid input. Please enter 'y' or 'n'.")
-                    
+                        print("Invalid input. Please enter 'y' or 'n'.")   
             else:
-                print('Invalid input. Please enter an 8-digit number or an interactable command.\n')
+                print("Invalid input. Please enter an 8-digit number")
+        else:
+            print("Invalid input. Please enter 'y' or 'n'.")
 
         print ("The ceiling suddenly opens up and a spike trap comes hurtling down killing you.")
+        quit() # on death quit
+
 
 
     def puzzleClue(self):
-        choice = input('You see a book that looks like it relates to the door on the right. Read it? (y/n): ')
+        choice = input('You see a book that looks like it relates to the door on the right. Read it? (y/n)\n')
         if choice == 'y':
         # Read the random number from the CSV file and provide the first digit as a clue
             with open('random_number.csv', mode='r') as file:
@@ -148,13 +163,16 @@ class rooms:
             print('You return to the center of the room')
 
     # MAIN ROOM 
-    def createRoom(self):
-        choice = input("You are in a room filled with torches with a few choices of direction. Do you go 'forward' 'back' 'left' or 'right'?: ")
-        if choice == "forward":
-            self.scaryRoom()
-        elif choice == 'back':
-            print("You attempt to use the back door of the room but it opens into a dead end")
-        elif choice == 'right':
-            self.skeletalRoom()
-        elif choice == 'left':
-            self.puzzleRoom()
+    def createRoom(self, instance):
+        while True:
+            choice = input("You are in a room filled with torches with a few choices of direction. Do you go 'forward' 'back' 'left' or 'right'?\n")
+            if choice == "forward":
+                self.scaryRoom()
+            elif choice == 'back':
+                print("You attempt to use the back door of the room but it opens into a dead end")
+            elif choice == 'right':
+                self.skeletalRoom()
+            elif choice == 'left':
+                self.puzzleRoom()
+            else:
+                print ("Please enter a valid input")
