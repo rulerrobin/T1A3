6536c7e209a2a3865965af245d80eae3a58fe96f
Genuel Mark <gmzapanta98@gmail.com>
2023-05-13 00:05:07 +1000

    tested

--- src/test.py
+++ src/test.py
@@ -12,39 +12,3 @@ def test_monster_room(monkeypatch, capsys):     # tests False weapon and fight
     captured = capsys.readouterr()  # Get printed output
     expected_output = "YOU HAVE NO WEAPON AND DIED"
     assert expected_output in captured.out
-
-def test_monster_room2(monkeypatch, capsys):    # tests False weapon and flee
-    # Test that the monsterRoom function works as expected
-    r = rooms()
-    r.player.weapon = False  # Set player weapon to False
-    monkeypatch.setattr('builtins.input', lambda _: "flee")  # Simulate player input
-
-    r.monsterRoom()
-
-    captured = capsys.readouterr()  # Get printed output
-    expected_output = "You escape the room and return to the previous location"
-    assert expected_output in captured.out
-
-def test_monster_room3(monkeypatch, capsys):
-    # Test that the monsterRoom function works as expected
-    r = rooms()
-    r.player.weapon = True  # Set player weapon to True
-    monkeypatch.setattr('builtins.input', lambda _: "fight")  # Simulate player input
-
-    r.monsterRoom()
-
-    captured = capsys.readouterr()  # Get printed output
-    expected_output = "YOU HAVE ESCAPED"
-    assert expected_output in captured.out
-
-def test_monster_room4(monkeypatch, capsys):
-    # Test that the monsterRoom function works as expected
-    r = rooms()
-    r.player.weapon = True  # Set player weapon to True
-    monkeypatch.setattr('builtins.input', lambda _: "flee")  # Simulate player input
-
-    r.monsterRoom()
-
-    captured = capsys.readouterr()  # Get printed output
-    expected_output = "You escape the room and return to the previous location"
-    assert expected_output in captured.out
